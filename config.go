cdf9926584d1b4ce69e58581076efa5ceaaba3d7
wrmn <aditya_wrmn@protonmail.com>
2021-01-13 08:24:32 +0700

    json with response msg

--- config.go
+++ config.go
@@ -2,7 +2,7 @@ package main
 
 // Config for Apache Kafka
 const (
-	broker = "localhost:9092"
+	broker = "188.166.255.175:9092"
 	group  = "test-go"
 	topic1 = "channel-kafka"
 	topic2 = "kafka-biller"
