d3763297c8b616144cf30ef392a6fe1d8f6fa212
wrmn <aditya_wrmn@protonmail.com>
2021-01-21 14:04:48 +0700

    mandatory handler

--- config.go
+++ config.go
@@ -2,7 +2,7 @@ package main
 
 // Config for Apache Kafka
 const (
-	broker = "188.166.255.175:29092"
+	broker = "localhost:9092"
 	group  = "Biller"
 	topic1 = "channelKafka2"
 	topic2 = "kafkaBiller2"
